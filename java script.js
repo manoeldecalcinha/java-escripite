5601c68f94a195c2e1998f5c2b89fb3a477514d7
Nicolas <160872991+manoeldecalcinha@users.noreply.github.com>
2024-09-17 12:09:49 -0300

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,19 @@
+//Tipo number
+
+//Var era utilizado para todas as váriavieis
+const numero01 = 100
+//let é uma atualiação do var e é possivel de varição
+const numero02 = 4
+//const é uma maneira de declarar uma variável que irá permanecer constante
+const numero03 = 7
+
+const soma = numero01 + numero02
+const subtracao = numero03 - numero02
+const multiplicacao = numero03 * numero01
+const divisao = numero01 / numero02
+
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
